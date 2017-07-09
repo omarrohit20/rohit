b4c12a53e97278698c2d5349ffc2f4ef5e43b116
rohit omar <omarrohit20@gmail.com>
2017-07-09 22:44:46 +0530

    changes for prediction

--- test/src/machine-learning/test.py
+++ test/src/machine-learning/test.py
@@ -1,11 +1,12 @@
+import quandl, math, time
 import pandas as pd
-import quandl
-import math
-import time
 import numpy as np
-from pymongo import MongoClient
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 from talib.abstract import *
+from pymongo import MongoClient
 from multiprocessing.dummy import Pool as ThreadPool
+from pip.req.req_file import preprocess
 
 connection = MongoClient('localhost', 27017)
 db = connection.Nsedata
@@ -37,150 +38,177 @@ def ta_lib_data(scrip):
         'volume': hsquantity,
         'turnover':hsturnover
     })
-    df = df[['date','open','high','low','close','volume','turnover']]
+    df = df[['open','high','low','close','volume','turnover']]
+    dfp = df[['open','high','low','close','volume','turnover']]
         
     if (df is not None):
         df.columns = df.columns.str.lower()
         df=df.rename(columns = {'total trade quantity':'volume'})
         df=df.rename(columns = {'turnover (lacs)': 'turnover'})
-        df['PCT_change'] = ((df['close'] - df['open'])/df['open'])*100
+        dfp['PCT_change'] = ((df['close'] - df['open'])/df['open'])*100
         
-        df['ADX'] = ADX(df) #Average Directional Movement Index http://www.investopedia.com/terms/a/adx.asp
-        df['ADXR'] = ADXR(df) #Average Directional Movement Index Rating https://www.scottrade.com/knowledge-center/investment-education/research-analysis/technical-analysis/the-indicators/average-directional-movement-index-rating-adxr.html
-        df['APO'] = APO(df) #Absolute Price Oscillator https://www.fidelity.com/learning-center/trading-investing/technical-analysis/technical-indicator-guide/apo
+        dfp['ADX'] = ADX(df) #Average Directional Movement Index http://www.investopedia.com/terms/a/adx.asp
+        dfp['ADXR'] = ADXR(df) #Average Directional Movement Index Rating https://www.scottrade.com/knowledge-center/investment-education/research-analysis/technical-analysis/the-indicators/average-directional-movement-index-rating-adxr.html
+        dfp['APO'] = APO(df) #Absolute Price Oscillator https://www.fidelity.com/learning-center/trading-investing/technical-analysis/technical-indicator-guide/apo
         aroon = AROON(df) #Aroon http://www.investopedia.com/terms/a/aroon.asp
-        df['AROONUP'], df['AROONDOWN'] = aroon['aroonup'], aroon['aroondown']
-        df['AROONOSC'] = AROONOSC(df)
-        df['BOP'] = BOP(df) #Balance Of Power https://www.marketvolume.com/technicalanalysis/balanceofpower.asp
-        df['CCI'] = CCI(df) #Commodity Channel Index http://www.investopedia.com/articles/trading/05/041805.asp
-        df['CMO'] = CMO(df) #Chande Momentum Oscillator https://www.fidelity.com/learning-center/trading-investing/technical-analysis/technical-indicator-guide/cmo
-        df['DX'] = DX(df) #Directional Movement Index http://www.investopedia.com/terms/d/dmi.asp
+        dfp['AROONUP'], dfp['AROONDOWN'] = aroon['aroonup'], aroon['aroondown']
+        dfp['AROONOSC'] = AROONOSC(df)
+        dfp['BOP'] = BOP(df) #Balance Of Power https://www.marketvolume.com/technicalanalysis/balanceofpower.asp
+        dfp['CCI'] = CCI(df) #Commodity Channel Index http://www.investopedia.com/articles/trading/05/041805.asp
+        dfp['CMO'] = CMO(df) #Chande Momentum Oscillator https://www.fidelity.com/learning-center/trading-investing/technical-analysis/technical-indicator-guide/cmo
+        dfp['DX'] = DX(df) #Directional Movement Index http://www.investopedia.com/terms/d/dmi.asp
         macd = MACD(df)
-        df['MACD'], df['MACDSIGNAL'], df['MACDHIST'] = macd['macd'], macd['macdsignal'], macd['macdhist']
-        #df['MACDEXT'] = MACDEXT(df)
-        #df['MACDFIX'] = MACDFIX(df)
-        df['MFI'] = MFI(df)
-        df['MINUS_DI'] = MINUS_DI(df)
-        df['MINUS_DM'] = MINUS_DM(df)
-        df['MOM'] = MOM(df)
-        df['PLUS_DI'] = PLUS_DI(df)
-        df['PLUS_DM'] = PLUS_DM(df)
-        df['PPO'] = PPO(df)
-        df['ROC'] = ROC(df)
-        df['ROCP'] = ROCP(df)
-        df['ROCR'] = ROCR(df)
-        df['ROCR100'] = ROCR100(df)
-        df['RSI'] = RSI(df)
-        #df['STOCH'] = STOCH(df)
-        #df['STOCHF'] = STOCHF(df)
-        #df['STOCHRSI'] = STOCHRSI(df)
-        df['TRIX'] = TRIX(df)
-        df['ULTOSC'] = ULTOSC(df)
-        df['WILLR'] = WILLR(df)
+        dfp['MACD'], dfp['MACDSIGNAL'], dfp['MACDHIST'] = macd['macd'], macd['macdsignal'], macd['macdhist']
+        #dfp['MACDEXT'] = MACDEXT(df)
+        #dfp['MACDFIX'] = MACDFIX(df)
+        dfp['MFI'] = MFI(df)
+        dfp['MINUS_DI'] = MINUS_DI(df)
+        dfp['MINUS_DM'] = MINUS_DM(df)
+        dfp['MOM'] = MOM(df)
+        dfp['PLUS_DI'] = PLUS_DI(df)
+        dfp['PLUS_DM'] = PLUS_DM(df)
+        dfp['PPO'] = PPO(df)
+        dfp['ROC'] = ROC(df)
+        dfp['ROCP'] = ROCP(df)
+        dfp['ROCR'] = ROCR(df)
+        dfp['ROCR100'] = ROCR100(df)
+        dfp['RSI'] = RSI(df)
+        #dfp['STOCH'] = STOCH(df)
+        #dfp['STOCHF'] = STOCHF(df)
+        #dfp['STOCHRSI'] = STOCHRSI(df)
+        dfp['TRIX'] = TRIX(df)
+        dfp['ULTOSC'] = ULTOSC(df)
+        dfp['WILLR'] = WILLR(df)
         
         bbands = BBANDS(df)
-        df['BBANDSUPPER'], df['BBANDSMIDDLE'], df['BBANDSLOWER'] = bbands['upperband'], bbands['middleband'], bbands['lowerband']
+        dfp['BBANDSUPPER'], dfp['BBANDSMIDDLE'], dfp['BBANDSLOWER'] = bbands['upperband'], bbands['middleband'], bbands['lowerband']
         
-#       df['DEMA'] = DEMA(df)
-        df['EMA9'] = EMA(df,9)
-        df['EMA21'] = EMA(df,21)
-        df['EMA25'] = EMA(df,25)
-        df['EMA50'] = EMA(df,50)
-        df['EMA100'] = EMA(df,100)
-        df['EMA200'] = EMA(df,200)
-        df['HT_TRENDLINE'] = HT_TRENDLINE(df)
-        df['KAMA'] = KAMA(df)
-        df['MA'] = MA(df)
-        #df['MAMA'] = MAMA(df)
-        #df['MAVP'] = MAVP(df)
-        df['MIDPOINT'] = MIDPOINT(df)
-        df['MIDPRICE'] = MIDPRICE(df)
-        df['SAR'] = SAR(df)
-        df['SAREXT'] = SAREXT(df)
-        df['SMA'] = SMA(df)
-        df['SMA9'] = SMA(df, 9)
-        df['T3'] = T3(df)
-        df['TEMA'] = TEMA(df)
-        df['TRIMA'] = TRIMA(df)
-        df['WMA'] = WMA(df)
+#       dfp['DEMA'] = DEMA(df)
+        dfp['EMA9'] = EMA(df,9)
+        dfp['EMA21'] = EMA(df,21)
+        dfp['EMA25'] = EMA(df,25)
+        dfp['EMA50'] = EMA(df,50)
+        dfp['EMA100'] = EMA(df,100)
+        dfp['EMA200'] = EMA(df,200)
+        dfp['HT_TRENDLINE'] = HT_TRENDLINE(df)
+        dfp['KAMA'] = KAMA(df)
+        dfp['MA'] = MA(df)
+        #dfp['MAMA'] = MAMA(df)
+        #dfp['MAVP'] = MAVP(df)
+        dfp['MIDPOINT'] = MIDPOINT(df)
+        dfp['MIDPRICE'] = MIDPRICE(df)
+        dfp['SAR'] = SAR(df)
+        dfp['SAREXT'] = SAREXT(df)
+        dfp['SMA'] = SMA(df)
+        dfp['SMA9'] = SMA(df, 9)
+        dfp['T3'] = T3(df)
+        dfp['TEMA'] = TEMA(df)
+        dfp['TRIMA'] = TRIMA(df)
+        dfp['WMA'] = WMA(df)
 
 
-        df['CDL2CROWS'] = CDL2CROWS(df)
-        df['CDL3BLACKCROWS'] = CDL3BLACKCROWS(df)
-        df['CDL3INSIDE'] = CDL3INSIDE(df)
-        df['CDL3LINESTRIKE'] = CDL3LINESTRIKE(df)
-        df['CDL3OUTSIDE'] = CDL3OUTSIDE(df)
-        df['CDL3STARSINSOUTH'] = CDL3STARSINSOUTH(df)
-        df['CDL3WHITESOLDIERS'] = CDL3WHITESOLDIERS(df)
-        df['CDLABANDONEDBABY'] = CDLABANDONEDBABY(df)
-        df['CDLADVANCEBLOCK'] = CDLADVANCEBLOCK(df) #Bearish reversal. prior trend Upward. Look for three white candles in an upward price trend. On each candle, price opens within the body of the previous candle. The height of the shadows grow taller on the last two candles.
-        df['CDLBELTHOLD'] = CDLBELTHOLD(df) # Bearish reversal. prior trend upward. Price opens at the high for the day and closes near the low, forming a tall black candle, often with a small lower shadow.
-        df['CDLBREAKAWAY'] = CDLBREAKAWAY(df) # Bearish reversal. prior trend upward. Look for 5 candle lines in an upward price trend with the first candle being a tall white one. The second day should be a white candle with a gap between the two bodies, but the shadows can overlap. Day three should have a higher close and the candle can be any color. Day 4 shows a white candle with a higher close. The last day is a tall black candle with a close within the gap between the bodies of the first two candles.
-        df['CDLCLOSINGMARUBOZU'] = CDLCLOSINGMARUBOZU(df)
-        df['CDLCONCEALBABYSWALL'] = CDLCONCEALBABYSWALL(df)
-        df['CDLCOUNTERATTACK'] = CDLCOUNTERATTACK(df)
-        df['CDLDARKCLOUDCOVER'] = CDLDARKCLOUDCOVER(df)
-        df['CDLDOJI'] = CDLDOJI(df)
-        df['CDLDOJISTAR'] = CDLDOJISTAR(df)
-        df['CDLDRAGONFLYDOJI'] = CDLDRAGONFLYDOJI(df)
-        df['CDLENGULFING'] = CDLENGULFING(df)
-        df['CDLEVENINGDOJISTAR'] = CDLEVENINGDOJISTAR(df)
-        df['CDLEVENINGSTAR'] = CDLEVENINGSTAR(df)
-        df['CDLGAPSIDESIDEWHITE'] = CDLGAPSIDESIDEWHITE(df)
-        df['CDLGRAVESTONEDOJI'] = CDLGRAVESTONEDOJI(df)
-        df['CDLHAMMER'] = CDLHAMMER(df)
-        df['CDLHANGINGMAN'] = CDLHANGINGMAN(df)
-        df['CDLHARAMI'] = CDLHARAMI(df)
-        df['CDLHARAMICROSS'] = CDLHARAMICROSS(df)
-        df['CDLHIGHWAVE'] = CDLHIGHWAVE(df)
-        df['CDLHIKKAKE'] = CDLHIKKAKE(df)
-        df['CDLHIKKAKEMOD'] = CDLHIKKAKEMOD(df)
-        df['CDLHOMINGPIGEON'] = CDLHOMINGPIGEON(df)
-        df['CDLIDENTICAL3CROWS'] = CDLIDENTICAL3CROWS(df)
-        df['CDLINNECK'] = CDLINNECK(df)
-        df['CDLINVERTEDHAMMER'] = CDLINVERTEDHAMMER(df)
-        df['CDLKICKING'] = CDLKICKING(df)
-        df['CDLKICKINGBYLENGTH'] = CDLKICKINGBYLENGTH(df)
-        df['CDLLADDERBOTTOM'] = CDLLADDERBOTTOM(df)
-        df['CDLLONGLEGGEDDOJI'] = CDLLONGLEGGEDDOJI(df)
-        df['CDLLONGLINE'] = CDLLONGLINE(df)
-        df['CDLMARUBOZU'] = CDLMARUBOZU(df)
-        df['CDLMATCHINGLOW'] = CDLMATCHINGLOW(df)
-        df['CDLMATHOLD'] = CDLMATHOLD(df)
-        df['CDLMORNINGDOJISTAR'] = CDLMORNINGDOJISTAR(df)
-        df['CDLMORNINGSTAR'] = CDLMORNINGSTAR(df)
-        df['CDLONNECK'] = CDLONNECK(df)
-        df['CDLPIERCING'] = CDLPIERCING(df)
-        df['CDLRICKSHAWMAN'] = CDLRICKSHAWMAN(df)
-        df['CDLRISEFALL3METHODS'] = CDLRISEFALL3METHODS(df)
-        df['CDLSEPARATINGLINES'] = CDLSEPARATINGLINES(df)
-        df['CDLSHOOTINGSTAR'] = CDLSHOOTINGSTAR(df)
-        df['CDLSHORTLINE'] = CDLSHORTLINE(df)
-        df['CDLSPINNINGTOP'] = CDLSPINNINGTOP(df)
-        df['CDLSTALLEDPATTERN'] = CDLSTALLEDPATTERN(df)
-        df['CDLSTICKSANDWICH'] = CDLSTICKSANDWICH(df)
-        df['CDLTAKURI'] = CDLTAKURI(df)
-        df['CDLTASUKIGAP'] = CDLTASUKIGAP(df)
-        df['CDLTHRUSTING'] = CDLTHRUSTING(df)
-        df['CDLTRISTAR'] = CDLTRISTAR(df)
-        df['CDLUNIQUE3RIVER'] = CDLUNIQUE3RIVER(df)
-        df['CDLUPSIDEGAP2CROWS'] = CDLUPSIDEGAP2CROWS(df)
-        df['CDLXSIDEGAP3METHODS'] = CDLXSIDEGAP3METHODS(df)
+        dfp['CDL2CROWS'] = CDL2CROWS(df)
+        dfp['CDL3BLACKCROWS'] = CDL3BLACKCROWS(df)
+        dfp['CDL3INSIDE'] = CDL3INSIDE(df)
+        dfp['CDL3LINESTRIKE'] = CDL3LINESTRIKE(df)
+        dfp['CDL3OUTSIDE'] = CDL3OUTSIDE(df)
+        dfp['CDL3STARSINSOUTH'] = CDL3STARSINSOUTH(df)
+        dfp['CDL3WHITESOLDIERS'] = CDL3WHITESOLDIERS(df)
+        dfp['CDLABANDONEDBABY'] = CDLABANDONEDBABY(df)
+        dfp['CDLADVANCEBLOCK'] = CDLADVANCEBLOCK(df) #Bearish reversal. prior trend Upward. Look for three white candles in an upward price trend. On each candle, price opens within the body of the previous candle. The height of the shadows grow taller on the last two candles.
+        dfp['CDLBELTHOLD'] = CDLBELTHOLD(df) # Bearish reversal. prior trend upward. Price opens at the high for the day and closes near the low, forming a tall black candle, often with a small lower shadow.
+        dfp['CDLBREAKAWAY'] = CDLBREAKAWAY(df) # Bearish reversal. prior trend upward. Look for 5 candle lines in an upward price trend with the first candle being a tall white one. The second day should be a white candle with a gap between the two bodies, but the shadows can overlap. Day three should have a higher close and the candle can be any color. Day 4 shows a white candle with a higher close. The last day is a tall black candle with a close within the gap between the bodies of the first two candles.
+        dfp['CDLCLOSINGMARUBOZU'] = CDLCLOSINGMARUBOZU(df)
+        dfp['CDLCONCEALBABYSWALL'] = CDLCONCEALBABYSWALL(df)
+        dfp['CDLCOUNTERATTACK'] = CDLCOUNTERATTACK(df)
+        dfp['CDLDARKCLOUDCOVER'] = CDLDARKCLOUDCOVER(df)
+        dfp['CDLDOJI'] = CDLDOJI(df)
+        dfp['CDLDOJISTAR'] = CDLDOJISTAR(df)
+        dfp['CDLDRAGONFLYDOJI'] = CDLDRAGONFLYDOJI(df)
+        dfp['CDLENGULFING'] = CDLENGULFING(df)
+        dfp['CDLEVENINGDOJISTAR'] = CDLEVENINGDOJISTAR(df)
+        dfp['CDLEVENINGSTAR'] = CDLEVENINGSTAR(df)
+        dfp['CDLGAPSIDESIDEWHITE'] = CDLGAPSIDESIDEWHITE(df)
+        dfp['CDLGRAVESTONEDOJI'] = CDLGRAVESTONEDOJI(df)
+        dfp['CDLHAMMER'] = CDLHAMMER(df)
+        dfp['CDLHANGINGMAN'] = CDLHANGINGMAN(df)
+        dfp['CDLHARAMI'] = CDLHARAMI(df)
+        dfp['CDLHARAMICROSS'] = CDLHARAMICROSS(df)
+        dfp['CDLHIGHWAVE'] = CDLHIGHWAVE(df)
+        dfp['CDLHIKKAKE'] = CDLHIKKAKE(df)
+        dfp['CDLHIKKAKEMOD'] = CDLHIKKAKEMOD(df)
+        dfp['CDLHOMINGPIGEON'] = CDLHOMINGPIGEON(df)
+        dfp['CDLIDENTICAL3CROWS'] = CDLIDENTICAL3CROWS(df)
+        dfp['CDLINNECK'] = CDLINNECK(df)
+        dfp['CDLINVERTEDHAMMER'] = CDLINVERTEDHAMMER(df)
+        dfp['CDLKICKING'] = CDLKICKING(df)
+        dfp['CDLKICKINGBYLENGTH'] = CDLKICKINGBYLENGTH(df)
+        dfp['CDLLADDERBOTTOM'] = CDLLADDERBOTTOM(df)
+        dfp['CDLLONGLEGGEDDOJI'] = CDLLONGLEGGEDDOJI(df)
+        dfp['CDLLONGLINE'] = CDLLONGLINE(df)
+        dfp['CDLMARUBOZU'] = CDLMARUBOZU(df)
+        dfp['CDLMATCHINGLOW'] = CDLMATCHINGLOW(df)
+        dfp['CDLMATHOLD'] = CDLMATHOLD(df)
+        dfp['CDLMORNINGDOJISTAR'] = CDLMORNINGDOJISTAR(df)
+        dfp['CDLMORNINGSTAR'] = CDLMORNINGSTAR(df)
+        dfp['CDLONNECK'] = CDLONNECK(df)
+        dfp['CDLPIERCING'] = CDLPIERCING(df)
+        dfp['CDLRICKSHAWMAN'] = CDLRICKSHAWMAN(df)
+        dfp['CDLRISEFALL3METHODS'] = CDLRISEFALL3METHODS(df)
+        dfp['CDLSEPARATINGLINES'] = CDLSEPARATINGLINES(df)
+        dfp['CDLSHOOTINGSTAR'] = CDLSHOOTINGSTAR(df)
+        dfp['CDLSHORTLINE'] = CDLSHORTLINE(df)
+        dfp['CDLSPINNINGTOP'] = CDLSPINNINGTOP(df)
+        dfp['CDLSTALLEDPATTERN'] = CDLSTALLEDPATTERN(df)
+        dfp['CDLSTICKSANDWICH'] = CDLSTICKSANDWICH(df)
+        dfp['CDLTAKURI'] = CDLTAKURI(df)
+        dfp['CDLTASUKIGAP'] = CDLTASUKIGAP(df)
+        dfp['CDLTHRUSTING'] = CDLTHRUSTING(df)
+        dfp['CDLTRISTAR'] = CDLTRISTAR(df)
+        dfp['CDLUNIQUE3RIVER'] = CDLUNIQUE3RIVER(df)
+        dfp['CDLUPSIDEGAP2CROWS'] = CDLUPSIDEGAP2CROWS(df)
+        dfp['CDLXSIDEGAP3METHODS'] = CDLXSIDEGAP3METHODS(df)
 
-        df['AVGPRICE'] = AVGPRICE(df)
-        df['MEDPRICE'] = MEDPRICE(df)
-        df['TYPPRICE'] = TYPPRICE(df)
-        df['WCLPRICE'] = WCLPRICE(df)
+        dfp['AVGPRICE'] = AVGPRICE(df)
+        dfp['MEDPRICE'] = MEDPRICE(df)
+        dfp['TYPPRICE'] = TYPPRICE(df)
+        dfp['WCLPRICE'] = WCLPRICE(df)
 
-#         df['ATR'] = ATR(df)
-#         df['NATR'] = NATR(df)
-#         df['TRANGE'] = TRANGE(df)
+#         dfp['ATR'] = ATR(df)
+#         dfp['NATR'] = NATR(df)
+#         dfp['TRANGE'] = TRANGE(df)
         
-        df['AD'] = AD(df)
-        df['ADOSC'] = ADOSC(df)
-        df['OBV'] = OBV(df)
+        dfp['AD'] = AD(df)
+        dfp['ADOSC'] = ADOSC(df)
+        dfp['OBV'] = OBV(df)
         
-        df.to_csv('temp/' + scrip + '.csv', encoding='utf-8')
+        forecast_col = 'PCT_change'
+        dfp.fillna(-99999, inplace=True)
+        forecast_out = 1
+        dfp['label'] = dfp[forecast_col].shift(-forecast_out)
+        
+    dfp.to_csv('temp/' + scrip + '.csv', encoding='utf-8')    
+    #print 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    #print '--------', scrip, '----------'
+    X = np.array(dfp.drop(['label'], 1))
+    X = preprocessing.scale(X)
+    X = X[:-forecast_out]
+    X_lately = X[-forecast_out:]
+    
+    dfp.dropna(inplace=True)
+    y = np.array(dfp['label'])
+    
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.1)
+    clf = LinearRegression()
+    clf.fit(X_train, y_train)
+    accuracy = clf.score(X_test, y_test)
+    forecast_set = clf.predict(X_lately)
+    print(scrip, accuracy, forecast_set)
+    #print 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'    
+        
+        
+        
+        #df.to_csv('temp/' + scrip + '.csv', encoding='utf-8')
              
 def calculateParallel(threads=2):
     pool = ThreadPool(threads)
