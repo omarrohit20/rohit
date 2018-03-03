0474b50fa24ccd5e9cab5b2cc0bcc9a157176e8c
rohit omar <omarrohit20@gmail.com>
2018-03-03 10:29:29 +0530

    removed kachra

--- src/machine-learning/regression_run_others.sh
+++ src/machine-learning/regression_run_others.sh
@@ -2,6 +2,7 @@ pyenv local system
 python ./technical.py all No
 pyenv local 3.5.0
 python ./regression-high.py all No
+pyenv local 3.5.0
 python ./regression-low.py all No
 python ./regression-result.py all No
 pyenv local system
