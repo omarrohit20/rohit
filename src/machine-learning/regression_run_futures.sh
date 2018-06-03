ca164c213d5d9e9324388868c17539de656de72c
rohit omar <omarrohit20@gmail.com>
2018-06-03 09:41:45 +0530

    code refactoring

--- src/machine-learning/regression_run_futures.sh
+++
@@ -1,8 +0,0 @@
-pyenv local system
-python ./technical.py all Yes
-pyenv local 3.5.0
-python ./regression-low.py all Yes
-pyenv local 3.5.0
-python ./regression-high.py all Yes
-python ./regression-result.py Yes
-pyenv local system
--- src/machine-learning/regression_run_others.sh
+++
@@ -1,8 +0,0 @@
-pyenv local system
-python ./technical.py all No
-pyenv local 3.5.0
-python ./regression-low.py all No
-pyenv local 3.5.0
-python ./regression-high.py all No
-python ./regression-result.py No
-pyenv local system
