ca79dc046d98521e64dd0f23a9558384f441381a
Nikita Rohit Gupta <omarrohit20@gmail.com>
2024-09-05 08:42:54 +0530

    fixes

--- src/chartlink_import/bpm_b_vol.py
+++ src/chartlink_import/bpm_b_vol.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     while (sb.nw <= time_11_00):
         if (sb.nw >= time_09_15 and sb.nw <= time_09_25):
-            # ( {33489} ( ( {cash} ( [=1] 5 minute volume > greatest(  1 day ago volume / 24  ) and [=1] 15 minute volume > greatest(  1 day ago volume / 18  ) and [=1] 5 minute volume > greatest(  2 days ago volume / 24  ) and [=1] 5 minute high > [=1] 5 minute sma ( [=1] 5 minute close , 200 ) and [=1] 5 minute close > [=1] 5 minute open and [=1] 5 minute close > 1 day ago close and ( ( ( [=1] 5 minute close - [=1] 5 minute open ) * 100 ) / 1 day ago close ) > 0.5 and ( ( ( [=1] 5 minute close - 1 day ago close ) * 100 ) / 1 day ago close ) > 0.5 and [=1] 5 minute open < [=-1] 30 minute high ) ) ) )
+            # ( {33489} ( ( {cash} ( [=1] 5 minute volume > greatest(  1 day ago volume / 24  ) and [=1] 10 minute volume > greatest(  1 day ago volume / 22  ) and [=1] 15 minute volume > greatest(  2 days ago volume / 15  ) and [=1] 5 minute high > [=1] 5 minute sma ( [=1] 5 minute close , 200 ) and [=1] 5 minute close > [=1] 5 minute open and [=1] 5 minute close > 1 day ago close and ( ( ( [=1] 5 minute close - [=1] 5 minute open ) * 100 ) / 1 day ago close ) > 0.5 and ( ( ( [=1] 5 minute close - 1 day ago close ) * 100 ) / 1 day ago close ) > 0.5 and [=1] 5 minute open < [=-1] 30 minute high ) ) ) )
             process_url_volBreakout('https://chartink.com/screener/morning-volume-breakout-1-2', 'morning-volume-breakout-buy', time_09_00, time_09_45)
 
         if (sb.nw >= time_09_20 and sb.nw <= time_09_45):
