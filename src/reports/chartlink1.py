12332e7a23a0537ae804fa05d5e1b722909a51a1
Nikita Rohit Gupta <omarrohit20@gmail.com>
2026-01-05 11:42:23 +0530

    fixes

--- src/reports/chartlink1.py
+++ src/reports/chartlink1.py
@@ -36,6 +36,7 @@ def main():
                 ((df['PCT_day_change'] < 0.5) | (df['PCT_day_change_pre1'] < 0.5) | (df['PCT_day_change_pre2'] < 0.5)) &
                 ((df['PCT_day_change'] < 2) | (df['PCT_day_change_pre1'] < 2)) &
                 ((df['PCT_day_change'] > -0.3) | (df['PCT_day_change_pre2'] > -0.3)) &
+                ((df['yearHighChange'] < 0) | (df['monthHighChange'] < 0)) &
                 (
                     ((df['systemtime'].str.contains('9:', case=False, regex=True, na=False)) & (df['forecast_day_PCT10_change'] > 2) & (df['forecast_day_PCT10_change'] < 7)) |
                     ((df['forecast_day_PCT10_change'] < -6) & (df['forecast_day_PCT5_change'] < 0)) |
@@ -53,6 +54,7 @@ def main():
                 ((df['PCT_day_change'] < 0.5) | (df['PCT_day_change_pre1'] < 0.5) | (df['PCT_day_change_pre2'] < 0.5)) &
                 ((df['PCT_day_change'] < 2) | (df['PCT_day_change_pre1'] < 2)) &
                 ((df['PCT_day_change'] > -0.3) | (df['PCT_day_change_pre2'] > -0.3)) &
+                ((df['yearHighChange'] < 0) | (df['monthHighChange'] < 0)) &
                 (
                     ((df['systemtime'].str.contains('9:', case=False, regex=True, na=False)) & (df['forecast_day_PCT10_change'] > 2) & (df['forecast_day_PCT10_change'] < 15)) |
                     ((df['systemtime'].str.contains('9:', case=False, regex=True, na=False)) & (df['forecast_day_PCT10_change'] > -1) & (df['forecast_day_PCT7_change'] > -1) & (df['forecast_day_PCT5_change'] > -1) & (df['forecast_day_PCT10_change'] < 7)) |
@@ -71,6 +73,7 @@ def main():
                 ((df['PCT_day_change'] < 0.5) | (df['PCT_day_change_pre1'] < 0.5) | (df['PCT_day_change_pre2'] < 0.5)) &
                 ((df['PCT_day_change'] < 2) | (df['PCT_day_change_pre1'] < 2)) &
                 ((df['PCT_day_change'] > -0.3) | (df['PCT_day_change_pre2'] > -0.3)) &
+                ((df['yearHighChange'] < 0) | (df['monthHighChange'] < 0)) &
                 (
                     ((df['systemtime'].str.contains('9:', case=False, regex=True, na=False)) & (df['forecast_day_PCT10_change'] > 2) & (df['forecast_day_PCT10_change'] < 7) & (df['yearHighChange'] <= -10) & (df['yearHighChange'] >= -70) & (df['PCT_day_change'] <= 1.5)) |
                     (((df['forecast_day_PCT10_change'] < -6) & (df['forecast_day_PCT5_change'] < 0))) |
@@ -88,6 +91,7 @@ def main():
                 ((df['PCT_day_change'] > -0.5) | (df['PCT_day_change_pre1'] > -0.5) | (df['PCT_day_change_pre2'] > -0.5)) &
                 ((df['PCT_day_change'] > -2) | (df['PCT_day_change_pre1'] > -2)) &
                 ((df['PCT_day_change'] < 0.3) | (df['PCT_day_change_pre2'] < 0.3)) &
+                ((df['yearLowChange'] > 0) | (df['monthLowChange'] > 0)) &
                 (
                     ((df['systemtime'].str.contains('9:', case=False, regex=True, na=False)) & (df['forecast_day_PCT10_change'] < -2) & (df['forecast_day_PCT10_change'] > -7)) |
                     (((df['forecast_day_PCT10_change'] > 6) & (df['forecast_day_PCT5_change'] > 0))) |
@@ -105,6 +109,7 @@ def main():
                 ((df['PCT_day_change'] > -0.5) | (df['PCT_day_change_pre1'] > -0.5) | (df['PCT_day_change_pre2'] > -0.5)) &
                 ((df['PCT_day_change'] > -2) | (df['PCT_day_change_pre1'] > -2)) &
                 ((df['PCT_day_change'] < 0.3) | (df['PCT_day_change_pre2'] < 0.3)) &
+                ((df['yearLowChange'] > 0) | (df['monthLowChange'] > 0)) &
                 (
                     ((df['systemtime'].str.contains('9:', case=False, regex=True, na=False)) & (df['forecast_day_PCT10_change'] < -2) & (df['forecast_day_PCT10_change'] > -15)) |
                     ((df['systemtime'].str.contains('9:', case=False, regex=True, na=False)) & (df['forecast_day_PCT10_change'] < 1) & (df['forecast_day_PCT7_change'] < 1) & (df['forecast_day_PCT5_change'] < 1) & (df['forecast_day_PCT10_change'] > -7)) |
@@ -123,6 +128,7 @@ def main():
                 ((df['PCT_day_change'] > -0.5) | (df['PCT_day_change_pre1'] > -0.5) | (df['PCT_day_change_pre2'] > -0.5)) &
                 ((df['PCT_day_change'] > -2) | (df['PCT_day_change_pre1'] > -2)) &
                 ((df['PCT_day_change'] < 0.3) | (df['PCT_day_change_pre2'] < 0.3)) &
+                ((df['yearLowChange'] > 0) | (df['monthLowChange'] > 0)) &
                 (
                     ((df['systemtime'].str.contains('9:', case=False, regex=True, na=False)) & (df['forecast_day_PCT10_change'] < -2) & (df['forecast_day_PCT10_change'] > -7) & (df['yearLowChange'] > 10) & (df['yearLowChange'] <= 70) & (df['PCT_day_change'] >= -1.5)) |
                     (((df['forecast_day_PCT10_change'] > 6) & (df['forecast_day_PCT5_change'] > 0))) |
