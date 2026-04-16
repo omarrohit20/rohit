6e62f7f1acb8c1ec56b102c42e2a2a5b3a9b583e
Nikita Rohit Gupta <omarrohit20@gmail.com>
2026-04-16 20:18:16 +0530

    fixes

---
+++ src/reports-sentiments.bat
@@ -0,0 +1,17 @@
+REM Change directory to the specified path, including a different drive
+CD /D "reports"
+REM Any commands after this line will execute in the new directory
+echo Now in directory: %CD%
+
+CD /D "../sentiment"
+REM Any commands after this line will execute in the new directory
+echo Now in directory: %CD%
+
+REM Start the streamlit script
+start "" /B streamlit run sentiment_analysis.py
+
+ECHO All scripts have been launched.
+
+
+
+PAUSE
